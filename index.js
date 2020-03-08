cd7a2a524189850f19fb6b5ecb5ed02bf44879d6
Kelsey Dickson <kelseymedved@gmail.com>
2020-03-08 13:27:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
 function  deleteFromObjectByKey(object, key){
   // delete key.object;
   delete object[key];
-  return key.object;
+  return object;
 }
