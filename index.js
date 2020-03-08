a38e3332e26340e154650ed4ac4d5ed7bd666eb8
Kelsey Dickson <kelseymedved@gmail.com>
2020-03-08 13:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateObjectWithKeyAndValue(object,key,value){
 return Object.assign({}, object, { [key]: value })
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key) {
   return Object.assign({}, object, key);
 }
